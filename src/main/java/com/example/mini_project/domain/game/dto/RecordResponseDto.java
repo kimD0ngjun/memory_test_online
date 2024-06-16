f65d47740f81f8de8329e0749446d889634be722
kimD0ngjun <chickentasty0112@gmail.com>
2024-06-16 20:04:53 +0900

    feat: 컨트롤러 일부 구현 및 DTO 필드 추가

--- src/main/java/com/example/mini_project/domain/game/dto/RecordResponseDto.java
+++ src/main/java/com/example/mini_project/domain/game/dto/RecordResponseDto.java
@@ -7,11 +7,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RecordResponseDto {
+    private Long id;
     private int level;
     private int gameScore;
     private String registration;
 
     public RecordResponseDto(Record record) {
+        this.id = record.getId();
         this.level = record.getLevel();
         this.gameScore = record.getGameScore();
         this.registration = record.getCreatedAt().toString();
