c21bd8baf1f14899bb50c41895ec9084eb46fd75
kimD0ngjun <chickentasty0112@gmail.com>
2024-06-20 02:02:26 +0900

    feat: 타임리프 문법 기반 랭킹 클라이언트 할당

--- src/main/java/com/example/mini_project/domain/page/PageController.java
+++ src/main/java/com/example/mini_project/domain/page/PageController.java
@@ -1,14 +1,21 @@
 package com.example.mini_project.domain.page;
 
+import com.example.mini_project.domain.game.dto.RankingResponseDto;
+import com.example.mini_project.domain.game.service.RankingRedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class PageController {
+
+    private final RankingRedisService rankingService;
+    private static final int TOP_RANKING = 10;
 
     @GetMapping
     public String getAuthPage() {
@@ -16,7 +23,10 @@ public class PageController {
     }
 
     @GetMapping("/game")
-    public String getGamePage() {
+    public String getGamePage(Model model) {
+        List<RankingResponseDto> rankings = rankingService.getTopRankings(TOP_RANKING);
+        model.addAttribute("rankings", rankings);
+
         return "game";
     }
 
