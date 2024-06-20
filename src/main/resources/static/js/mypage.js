52c07563912753d1fa6b2605c8f3f1b2a7efbbcc
kimD0ngjun <chickentasty0112@gmail.com>
2024-06-20 15:46:25 +0900

    feat: 개인 기록 삭제 연동 구축

---
+++ src/main/resources/static/js/mypage.js
@@ -0,0 +1,40 @@
+// 기록 삭제
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.deleteButton').forEach(button => {
+        button.addEventListener('click', () => {
+            const parentLi = button.closest('li');
+            const hiddenElement = parentLi.querySelector('.hidden');
+            const date = parentLi.querySelector('.date').textContent
+            const id = hiddenElement.textContent;
+            console.log('삭제 아이디:', id);
+            console.log('삭제하려는 기록 날짜:', date)
+
+            const check = confirm(`삭제하려는 기록 날짜: ${date}\n\n기록을 삭제하시겠습니까?\n(이미 랭킹에 등재됐으면 기록만 삭제되고 랭킹은 삭제되지 않습니다.)`);
+
+            if (check) {
+                fetch(`/mini/game/record?id=${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok ' + response.statusText);
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('삭제 성공:', data);
+                        parentLi.remove();
+                        alert(data.message); // Show success message
+                    })
+                    .catch(error => {
+                        console.error('삭제 에러:', error);
+                        alert('삭제가 되지 않았습니다. 관리자에게 문의하세요.');
+                    });
+            }
+        });
+    });
+});
