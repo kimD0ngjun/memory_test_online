8aa227ce9924329005c14253f559aa3aff0fadce
kimD0ngjun <chickentasty0112@gmail.com>
2024-06-23 12:43:22 +0900

    feat: 스네이크 게임 랭킹 서비스 작성

--- src/main/java/com/example/mini_project/domain/game/common/dto/RankingResponseDto.java
+++ src/main/java/com/example/mini_project/domain/game/common/dto/RankingResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.mini_project.domain.game.common.dto;
 
 import com.example.mini_project.domain.game.memory.entity.MemoryTestRanking;
+import com.example.mini_project.domain.game.snake.entity.SnakeGameRanking;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,5 +24,13 @@ public class RankingResponseDto {
         this.email = memoryTestRanking.getEmail();
         this.level = memoryTestRanking.getLevel();
         this.gameScore = memoryTestRanking.getGameScore() + " 점";
+    }
+
+    public RankingResponseDto(SnakeGameRanking snakeGameRanking, int rank) {
+        this.rank = rank + "등";
+        this.username = snakeGameRanking.getUsername();
+        this.email = snakeGameRanking.getEmail();
+        this.level = snakeGameRanking.getLevel();
+        this.gameScore = snakeGameRanking.getGameScore() + " 점";
     }
 }
--- src/main/java/com/example/mini_project/domain/game/snake/service/SnakeGameRankingRedisService.java
+++ src/main/java/com/example/mini_project/domain/game/snake/service/SnakeGameRankingRedisService.java
@@ -1,11 +1,35 @@
 package com.example.mini_project.domain.game.snake.service;
 
+
 import com.example.mini_project.domain.game.common.dto.RankingResponseDto;
+import com.example.mini_project.domain.game.common.service.RankingRedisService;
+import com.example.mini_project.domain.game.snake.entity.SnakeGameRanking;
+import com.example.mini_project.domain.game.snake.repository.SnakeGameRankingRedisRepository;
 import com.example.mini_project.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
-public interface SnakeGameRankingRedisService {
-    void createRanking(User user, int level, int gameScore);
-    List<RankingResponseDto> getTopRankings(int topN);
+@Service
+@RequiredArgsConstructor
+public class SnakeGameRankingRedisService implements RankingRedisService {
+
+    private final SnakeGameRankingRedisRepository snakeGameRankingRedisRepository;
+
+    @Override
+    public void createRanking(User user, int level, int gameScore) {
+        SnakeGameRanking snakeGameRanking = new SnakeGameRanking(user.getEmail(), user.getUsername(), level, gameScore);
+
+        snakeGameRankingRedisRepository.save(snakeGameRanking);
+    }
+
+    @Override
+    public List<RankingResponseDto> getTopRankings(int topN) {
+        List<SnakeGameRanking> snakeGameRankings = snakeGameRankingRedisRepository.getRankingRange(0, topN - 1);
+
+        return IntStream.range(0, snakeGameRankings.size()).
+                mapToObj(i -> new RankingResponseDto(snakeGameRankings.get(i), i + 1)).toList();
+    }
 }
