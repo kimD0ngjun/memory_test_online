7a123e6ff3c63014d1b88ad055aa987a95a18a67
kimD0ngjun <chickentasty0112@gmail.com>
2024-06-27 15:10:04 +0900

    fix: 클라이언트 일부 수정

--- src/main/resources/static/js/snake_game.js
+++ src/main/resources/static/js/snake_game.js
@@ -134,7 +134,7 @@ async function game() {
 
     // 분기 단위시간 조정(줄일 수록 빡세겠지)
     // 60, hard는 45
-    setTimeout(game, 60);
+    setTimeout(game, 75);
 }
 
 // 게임 시작~
@@ -189,7 +189,7 @@ async function resetGame(stage, scoreCount) {
         await addScore(stage, scoreCount)
 
         // 2. 랭킹 등재여부 분기
-        let ranking = confirm(`최종 클리어 : ${stage} 단계\n총합 스코어 : ${scoreCount}\n\n기록을 랭킹에 등재하시겠습니까?`);
+        let ranking = confirm(`최종 클리어 : ${stage} 단계\n총합 스코어 : ${scoreCount}\n\n기록을 랭킹에 등재하시겠습니까?\n(기존 기록이 이미 랭킹에 등재된 경우, 가장 높은 레벨, 점수 합산 기록만이 등재됩니다)`);
 
         if (ranking) {
             await addRanking(stage, scoreCount);
